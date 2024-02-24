1cbcecc1ae934b736560546c0755aae62c77b35a
Waltair-Business-School <67534422+Waltair-Business-School@users.noreply.github.com>
2024-02-24 17:55:55 +0530

    logo

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import LogoImage from "../../Assets/logo.png"
+import styles from "./Logo.module.css"
 
 export default function Logo(){
     return (
+        <div className={styles.logo}>
         <img src={LogoImage} alt="qtify-logo" width={67} />
+        </div>
     );
 }
